18f2746b54f0011ed4baf1c4a45dd893a9f4fa0a
MichaelVL <michael.larsen@eficode.com>
2022-04-06 17:55:39 +0200

    Add metrics

--- dashboards/dashboard-requests-limits.py
+++ dashboards/dashboard-requests-limits.py
@@ -6,21 +6,53 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '.'))
 from common import *
 
 
-mem_requests = capacity_graph('Memory Requests',
+nodes_num = number('Nodes',
+                   [('sum(kube_node_info)')])
+
+pods_running = number('PODs Running',
+                      [('sum(kube_pod_status_phase{exported_namespace=~"$namespace", phase="Running"})')])
+
+pods_pending = number('PODs Pending',
+                      [('sum(kube_pod_status_phase{exported_namespace=~"$namespace", phase="Pending"})')])
+
+pods_restarting = number('PODs Failed',
+                      [('sum(kube_pod_status_phase{exported_namespace=~"$namespace", phase="Failed"})')],
+                         thresholds="1,2", colorValue=True)
+
+mem_requests = capacity_graph('Memory Requests Ratio',
                               [('sum(container_memory_working_set_bytes{container!="",namespace=~"$namespace"}) by (container,pod) / sum(kube_pod_container_resource_requests{resource="memory",exported_namespace=~"$namespace"}) by (container,pod)', '{{container}}')])
 
-mem_limits = capacity_graph('Memory Limits',
+mem_limits = capacity_graph('Memory Limits Ratio',
                             [('sum(container_memory_working_set_bytes{container!="",namespace=~"$namespace"}) by (container,pod) / sum(kube_pod_container_resource_limits{resource="memory",exported_namespace=~"$namespace"}) by (container,pod)', '{{container}}')])
+
+cpu_capacity = capacity_graph('Cluster CPU Requests',
+                              [('sum(kube_node_status_capacity{resource="cpu",exported_node=~"$node"})', 'allocatable'),
+                               ('sum(kube_node_status_allocatable{resource="cpu",exported_node=~"$node"})', 'capacity'),
+                               ('sum(kube_pod_container_resource_requests{resource="cpu",exported_node=~"$node"})', 'current')])
+
+mem_capacity = capacity_graph('Cluster Memory Requests',
+                              [('sum(kube_node_status_allocatable{resource="memory",exported_node=~"$node"})', 'allocatable'),
+                               ('sum(kube_node_status_capacity{resource="memory",exported_node=~"$node"})', 'capacity'),
+                               ('sum(kube_pod_container_resource_requests{resource="memory",exported_node=~"$node"})', 'current')])
 
 
 dashboard = Dashboard(
-    title=os.environ['DASHBOARD_TITLE'],
+    title='Resource Usage',
     #editable=False,
     templating = Templating(list=[
+        Template(name='node', dataSource=datasource, query='label_values(kubernetes_io_hostname)', includeAll=True, allValue='.*'),
         Template(name='namespace', dataSource=datasource, query='label_values(namespace)', includeAll=True, allValue='.*'),
     ]),
     rows = [
-        Row(title='PODs', collapse=False,
+        Row(title='Cluster Overview', collapse=False,
+            panels = [
+                nodes_num, pods_running, pods_pending, pods_restarting
+        ]),
+        Row(title='Cluster Resources', collapse=False,
+            panels = [
+                cpu_capacity, mem_capacity
+        ]),
+        Row(title='POD Resources', collapse=False,
             panels = [
                 mem_requests, mem_limits
         ]),
